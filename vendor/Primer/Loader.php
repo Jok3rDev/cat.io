200b5e3547ba1e77a1c9d3467495942ffdf34c3d
Fabrice Trimech <fabrice.trimech@gmail.com>
2016-05-06 09:41:03 +0200

    Appel de l'autoloader

--- vendor/Primer/Loader.php
+++ vendor/Primer/Loader.php
@@ -8,8 +8,17 @@
 
 namespace Primer;
 
+require_once '_Abstract\AbstractLoader.php';
 
-class Loader
+class Loader extends _Abstract\AbstractLoader
 {
+	protected function getVendorDirectory()
+	{
+		return parent::processPathDirectory(__DIR__, 1);
+	}
 	
+	protected function getApplicationDirectory()
+	{
+		return parent::processPathDirectory(__DIR__, 2, 'class');
+	}
 }
--- vendor/Primer/_Abstract/AbstractLoader.php
+++ vendor/Primer/_Abstract/AbstractLoader.php
@@ -6,29 +6,93 @@
  * Time: 10:25
  */
 
-namespace Primer;
+namespace Primer\_Abstract;
 
 
-class AbstractLoader
+abstract class AbstractLoader
 {
 	protected static $instance;
 	protected $classCollection = array();
+
+	const PARENT_DIRECTORY = '..';
 	const CLASS_EXTENSION = '.php';
 	const VENDOR_DIRECTORY = 'vendor';
 
-	abstract protected function getInstalleurDirectories();
+
 	abstract protected function getVendorDirectory();
 	abstract protected function getApplicationDirectory();
 
-	protected static function run() {
+	private static function run() {
 		if (!(static::$instance instanceof static)) {
 			static::$instance = new static;
 		}
-
 		return static::$instance;
 	}
 
-	protected static function register() {
-		return static::run();
+	public static function register() {
+		return static::run()->handlerClassDirectories()->autoload();
 	}
+
+	protected function handlerClassDirectories() {
+		$this->classCollection = $this->getDirectories();
+
+		return $this;
+	}
+
+	protected function getDirectories() {
+		return array_merge(
+			$this->getApplicationDirectory(),
+			$this->getVendorDirectory()
+		);
+	}
+
+	protected function processPathDirectory(string $base, int $numberOfBounds, $path = null) {
+		$pathParts = array();
+		$pathParts[] = $base;
+		for ($i = 0; $i < $numberOfBounds; $i++) {
+			$pathParts[] = self::PARENT_DIRECTORY;
+		}
+		if (!is_null($path)) {
+			$pathParts[] = $path;
+		}
+
+		return array(implode(DIRECTORY_SEPARATOR, $pathParts));
+	}
+
+	protected function autoload() {
+		spl_autoload_register(array($this, "myAutoload"));
+		return $this;
+	}
+
+	protected function myAutoload($className) {
+		$class = $this->sanitizeCLass($className);
+		$list = $this->getPath($class);
+		$this->load($list);
+	}
+
+	private function sanitizeCLass($name) {
+		return DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $name);
+	}
+
+	private function load($list) {
+		foreach ($list as $value) {
+			if (file_exists($value)) {
+				includeFile($value);
+				return true;
+			}
+		}
+	}
+
+	protected function getPath($pClassName) {
+		$retour = array();
+		foreach ($this->classCollection as $key => $value) {
+			$retour[] = $value . $pClassName. self::CLASS_EXTENSION;
+		}
+
+		return $retour;
+	}
+
+}
+function includeFile($file) {
+	require_once $file;
 }
